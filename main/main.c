a6b668b0fc2f8fc9e0fb3536ad1622a0b5418cf8
Nigdzie <46287652+Nigdzie@users.noreply.github.com>
2025-06-06 20:40:59 +0200

    Initialize NVS before starting WiFi

--- main/main.c
+++ main/main.c
@@ -16,6 +16,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/uart.h"
+#include "nvs_flash.h"
 
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
@@ -215,6 +216,13 @@ static void cli_task(void *pv){
 void app_main(void)
 {
     ESP_LOGD(TAG, "app_main started");
+    // Initialize NVS to store Wi-Fi calibration data
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     const uart_config_t uart_config = {
